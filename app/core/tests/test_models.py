1eba60dfdb879190dd2a534e2254990bfb9fc4e5
Fabrice Didierjean <Fabrice.Didierjean@hotmail.fr>
2019-11-16 15:21:46 +0100

    Test App

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creatinf a new user with an email is successful"""
+        email = 'test@coredmp.net'
+        password = 'TestPass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """Test that email for new user is normalize"""
+        email = 'test@COREDMP.NET'
+        user = get_user_model().objects.create_user(email, "test123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raise error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
