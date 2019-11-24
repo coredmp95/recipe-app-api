b60213598fa76d6832599abaf34cf3eec797e94b
Fabrice Didierjean <Fabrice.Didierjean@hotmail.fr>
2019-11-24 16:02:24 +0100

    Add Image upload featur

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -42,3 +42,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serilize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading image for recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_field = ('id', )
